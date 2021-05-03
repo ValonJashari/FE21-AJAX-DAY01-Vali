da2a7b5d4a075b973be50c2d635963803e56ca06
Valon Jashari <valonjashari18@gmail.com>
2021-05-03 13:25:26 +0200

    Basic_Intmerdiate

---
+++ ajax/users.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$conn=mysqli_connect('localhost','root','' ,'ajaxtest');
+
+
+//query
+
+    $query="SELECT * FROM  users";
+
+
+//Get results
+
+ $result= mysqli_query($conn,$query);
+
+
+ //Fetch Data from database
+
+ $users=mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+ echo json_encode($users); //js_encode is the php method for JSON.stringify
+
+
+?>
+
